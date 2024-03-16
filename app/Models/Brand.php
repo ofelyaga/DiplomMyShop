0277f203b72d024c8b6bc114e6305747f626a9a5
ofelya.galoyan <of4ok@mail.ru>
2024-03-16 23:09:23 +0300

    logger telegram bot

---
+++ app/Models/Brand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'slug',
+        'title',
+        'thumbnail'
+    ];
+
+    /**
+     * @var \Illuminate\Support\Stringable|mixed|__anonymous@6587
+     */
+
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function (Brand $brand) {
+            $brand->slug = $brand->slug ?? str($brand->title)->slug();
+        });
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
---
+++ database/migrations/2024_03_04_195130_create_products_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->string('title');
+            $table->foreignIdFor(Brand::class)
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->unsignedInteger('price')->default(0);
+            $table->string('thumbnail')->nullable();
+            $table->timestamps();
+        });
+        
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Category::class)
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->foreignIdFor(Product::class)
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (app()->isLocal()) {
+            Schema::dropIfExists('category_product');
+            Schema::dropIfExists('products');
+        }
+    }
+};
